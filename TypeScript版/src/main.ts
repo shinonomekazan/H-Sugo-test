37143ea5308a12f19b23d7463582d8fa0306d175
H-Sugo <mgrn0343@gmail.com>
2023-05-16 21:53:34 +0900

    シーン追加とシステム変更

--- TypeScript版/src/main.ts
+++ TypeScript版/src/main.ts
@@ -1,28 +1,39 @@
 import * as system from "./systems";
 
 function main(): void{
-	system.scene.onLoad.add(() => {
+	system.openingScene.onLoad.add(() => {
+		system.openingScene.append(system.ground0);
+		system.showDisplay(system.openingScene);
+		system.showRules(system.openingScene);
+		system.openingScene.onPointDownCapture.add(() => {
+			g.game.replaceScene(system.gameScene);
+		});
+	});
+
+	g.game.pushScene(system.openingScene);
+
+	system.gameScene.onLoad.add(() => {
 		system.bgmConfig();
 
 		// 地面生成
-		system.scene.append(system.ground);
+		system.gameScene.append(system.ground1);
+
+		system.showDisplay(system.gameScene);
 
 		// 虫生成
 		const insects = system.numberingInsects();
 
 		// 草生成
-		for (let i = 0; i < 7; i++) {
-			system.createGrass();
-		}
+		// for (let i = 0; i < 7; i++) {
+		// 	system.createGrass();
+		// }
 
 		// ステータス表示
-		system.statusDisplay();
+		// system.statusDisplay();
 
 		system.setGoal(insects, insects.length);
 
 	});
-
-	g.game.pushScene(system.scene);
 
 };
 
